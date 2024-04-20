d18727556bca2d19f8a1856f0600a4aba58005cc
Seray Fırıncıoğulları <158742686+serayfirinciogullari@users.noreply.github.com>
2024-04-20 20:42:18 +0300

    kitap ekleme bloğu eklendi

--- personel_girisi.py
+++ personel_girisi.py
@@ -1,12 +1,17 @@
 def personel_girisi(sifre):
+
     
     dosya_sifre = open("admin_sifre.txt","r") #metin dosyasında tutulan şifreyi okuyup dosya_sifre değişkeninde tutat
     satir = dosya_sifre.readline()
     dosya_sifre.close()
     if sifre == satir:  #oturum açma
         print("Oturum açıldı ^^")
-        sifre_degistirme = int(input("Şifre değiştirmek için 1, devam etmek için herhangi bir tuşa basınız:"))
-        if sifre_degistirme == 1:
+
+        print("1-Şifre değiştirme\n2-Kitap ekleme\n3-Kitap silme\n4-Kitap durumu güncelleme")
+
+        islem = int(input("Yapmak istediğiniz işlemi giriniz:"))
+
+        if islem == 1: #şifre değiştirme bloğu
             yeni_sifre = input("Yeni şifreyi giriniz:")
             yeni_sifre_kontrol = input("Şifreyi tekrar gir:")
 
@@ -16,10 +21,40 @@ def personel_girisi(sifre):
                 print("Şifre başarıyla değiştirildi")
                 dosya_guncelleme.close()
             else:
-                print("Lütfen tekrar deneyiniz")
-        else:
-            pass
-    else:
+                while True:
+                    deneme=input(f"Girilen şifreler uyuşmuyor, tekrar deneyin")
+                    if deneme==satir:
+                        print("Şifreniz güncellendi ^^")
+
+        
+        elif islem==2: #kitap ekleme bloğu
+            isim = input("Eklemek istediğiniz kitabın ismini giriniz: ") + "\n"
+            tur = input("Eklemek istediğiniz kitabın türünü giriniz: ") + "\n"
+            yayinevi = input("Eklemek istediğiniz kitabın yayınevini giriniz: ") + "\n"
+            yayin_yili = input("Eklemek istediğiniz kitabın yayın yılını giriniz: ") + "\n"
+            isbn = input("Eklemek istediğiniz kitabın ISBN numarasını giriniz: ") + "\n"
+
+            try:
+                with open("kitap_bilgileri.txt", "a", encoding="utf-8") as dosya:
+                    dosya.write(isim)
+                    dosya.write(tur)
+                    dosya.write(yayinevi)
+                    dosya.write(yayin_yili)
+                    dosya.write(isbn)
+                print("Bilgiler başarıyla eklendi")
+            except:
+                print("Bir hata oluştu")
+
+
+
+
+        
+        
+
+
+
+
+    else: #yanlış şifre girişi olunca çalışacak blok
         for i in range (3):
             deneme=input(f"Girdiğiniz şifre yanlış {3-i} deneme hakkınız kaldı")
             
