f192bde18f21386d17fe559b3994feb17021b370
Seray Fırıncıoğulları <158742686+serayfirinciogullari@users.noreply.github.com>
2024-04-23 14:01:51 +0300

    kitap ekleme fonksiyonu oluşturuldu

---
+++ kitap_ekle.py
@@ -0,0 +1,19 @@
+def kitap_ekle():
+
+    isim = input("Eklemek istediğiniz kitabın ismini giriniz: ") + "\n"
+    isbn = input("Eklemek istediğiniz kitabın ISBN numarasını giriniz: ") + "\n"
+    yazar=input("Eklemek istediğiniz kitabın yazarını giriniz: ") + "\n"
+    yayinevi = input("Eklemek istediğiniz kitabın yayınevini giriniz: ") + "\n"
+    yayin_yili = input("Eklemek istediğiniz kitabın yayın yılını giriniz: ") + "\n"
+    stok_durumu=input("Eklemek istediğiniz kitabın stok durumunu giriniz: ") + "\n"
+    tur = input("Eklemek istediğiniz kitabın türünü giriniz: ") + "\n"
+
+    with open("kitap_bilgileri.txt", "a+", encoding="utf-8") as dosya:
+        dosya.write(isim)
+        dosya.write(isbn)
+        dosya.write(yazar)
+        dosya.write(yayinevi)
+        dosya.write(yayin_yili)
+        dosya.write(stok_durumu)
+        dosya.write(tur)
+        print("Bilgiler başarıyla eklendi")
