98ad2ddfeb772f6ac13d3d4e86a90ec39f134393
omerArd <161716732+omerArd@users.noreply.github.com>
2024-04-25 01:40:32 +0300

    kitap ismi aratma ve bilgileri düzenli yazdırma
    artık kitap numarası girerek değil isim aratarak çalışıyo. Kitapların bilgilerini de hangi bilginin ne olduğunu başına yazıcak şekilde değiştirdim.

--- kitap_bilgisi.py
+++ kitap_bilgisi.py
@@ -1,15 +1,35 @@
-import linecache
- 
-def kitap_bilgisi(kitap_numarasi):
-    try:
-        dosya = "kitap_bilgileri.txt"
-        ilk_satir = (kitap_numarasi - 1) * 5 + 1
-        son_satir = ilk_satir + 4
-
-        for satir_numarasi in range(ilk_satir, son_satir + 1):
-            satir = linecache.getline(dosya, satir_numarasi)
-            print(satir.strip())  # Okunan satırları yazdır
-
-    except FileNotFoundError:
-        print("Dosya bulunamadı.")
-
+def kitap_bilgisi():
+    kitap_kontrol = False
+    while not kitap_kontrol:
+        kitap_ismi = input("Bilgilerini görüntülemek istediğiniz kitabın ismini giriniz: ").strip().upper()
+        sayac = 0
+        with open("kitap_bilgileri.txt", "r") as dosya:
+            while True:
+                satir = dosya.readline()
+                sayac += 1
+                if satir.strip() == kitap_ismi:
+                    print("\nGirdiğiniz kitap kütüphane arşivinde mevcuttur.\n")
+                    kitap_kontrol = True
+                    for i in range(1, 8):
+                        if i == 1:
+                            print("Kitabın ismi:",satir.strip())
+                        elif i == 2:
+                            print("Kitabın ISBN numarası:",satir.strip())
+                        elif i == 3:
+                            print("Kitabın yazarı:",satir.strip())
+                        elif i == 4:
+                            print("Kitabın yayınevi:",satir.strip())
+                        elif i == 5:
+                            print("Kitabın yayın yılı:", satir.strip())
+                        elif i == 6:
+                            print("Kitabın stok adedi:",satir.strip())
+                        else:
+                            print("Kitabın Türü:",satir.strip())
+                        satir = dosya.readline()
+                        sayac += 1
+                if not satir:
+                    break
+            if kitap_kontrol:
+                break
+        if not kitap_kontrol:
+            print("Girdiğiniz kitap kütüphane arşivinde bulunmamaktadır. Lütfen tekrar deneyiniz.")
