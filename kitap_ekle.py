b9587f4df7c8105927b78f1b8445bc813c472e0c
omerArd <161716732+omerArd@users.noreply.github.com>
2024-04-25 01:38:21 +0300

    stok kısmı değişti
    txt dosyasında stok var yerine stok adedi tuttuğumuz için ekleme kısmını da ona göre değiştirdim

--- kitap_ekle.py
+++ kitap_ekle.py
@@ -5,7 +5,7 @@ def kitap_ekle():
     yazar=input("Eklemek istediğiniz kitabın yazarını giriniz: ") + "\n"
     yayinevi = input("Eklemek istediğiniz kitabın yayınevini giriniz: ") + "\n"
     yayin_yili = input("Eklemek istediğiniz kitabın yayın yılını giriniz: ") + "\n"
-    stok_durumu=input("Eklemek istediğiniz kitabın stok durumunu giriniz: ") + "\n"
+    stok_adedi=input("Eklemek istediğiniz kitabın stok adedini giriniz: ") + "\n"
     tur = input("Eklemek istediğiniz kitabın türünü giriniz: ") + "\n"
 
     with open("kitap_bilgileri.txt", "a+", encoding="utf-8") as dosya:
@@ -14,6 +14,6 @@ def kitap_ekle():
         dosya.write(yazar)
         dosya.write(yayinevi)
         dosya.write(yayin_yili)
-        dosya.write(stok_durumu)
+        dosya.write(stok_adedi)
         dosya.write(tur)
         print("Bilgiler başarıyla eklendi")
